7798aee65a76882fe771c4d98eb2962a2395f949
httpjamesm <github@httpjames.space>
2023-09-11 17:24:47 -0400

    feat: stop string slices

--- inference_test.go
+++ inference_test.go
@@ -11,13 +11,13 @@ import (
 func TestNewInference(t *testing.T) {
 	client := NewClient(os.Getenv("TOGETHERAI_API_KEY"))
 
-	stopString := "*"
+	stopStrings := []string{"*"}
 
 	respBody, err := client.NewInference(InferenceConfig{
 		Model:     "togethercomputer/RedPajama-INCITE-7B-Instruct",
 		Prompt:    "The capital of France is",
 		MaxTokens: 128,
-		Stop:      &stopString,
+		Stop:      &stopStrings,
 	})
 	assert.NoError(t, err)
 	assert.NotNil(t, respBody)
